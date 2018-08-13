e3e6df8f46797df198d14642b3b88301a2f80c07
Faiz <faiz.ggmu@gmail.com>
2018-08-13 23:16:33 +0500

    Add and Delete implenented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,27 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css']
 })
 export class AppComponent {
-  title = 'ToDo Angular';
-  newItem = '';
-  addTodo (event) {
-   console.log(event);
+  title = 'Todo List';
+  newTodo = '';
+  todos = [];
+  todoObj: any;
+  // To add list in todo Array
+  addTodo(event) {
+
+     this.todoObj = {
+      item : this.newTodo,
+      complete : false
+    };
+    this.todos.push(this.todoObj);
+    this.newTodo = '';
+  }
+
+  // To mark as Done
+  delTodo(i) {
+      this.todos[i].complete = true;
   }
 }
